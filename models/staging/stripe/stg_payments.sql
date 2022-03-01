d57aed870b1a0d96579baf276ab68b6843e756c6
Andrés Campos <andres.campos@sdggroup.com>
2022-03-01 11:06:19 +0000

    [Adding files] Finishing the DBT Fundamentals course

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -5,10 +5,11 @@ with payments as (
         orderid as order_id,
         paymentmethod as payment_method,
         status as status,
+        -- amount is stored in cents, convert it to dollars
         amount / 100 as amount,
         created as created_at
 
-    from raw.stripe.payment
+    from {{ source('stripe', 'payment') }}
 
 )
 
